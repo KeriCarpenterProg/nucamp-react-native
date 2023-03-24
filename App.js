292619997a5803f42013b83451f127afd75228c0
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-03-23 18:56:03 -0700

    Components Part 2

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
 import Main from "./screens/MainComponent";
 
 export default function App() {
-  return <Main />;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "lightpink",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});

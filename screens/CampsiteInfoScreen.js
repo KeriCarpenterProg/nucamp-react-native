238f61126c404f1e04dcb37ab4bee37d69e27f2b
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-03-27 18:35:06 -0700

    new stuff

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,7 +1,11 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import COMMENTS from "../shared/comments";
 
 const CampsiteInfoScreen = ({ route }) => {
   const { campsite } = route.params;
+  const [comments, setComments] = useState(COMMENTS);
   return <RenderCampsite campsite={campsite} />;
 };
 

ddefdea44f56f8481b15e185c578b1d39b689c85
KeriCarpenterProg <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-04-01 10:08:26 -0500

    through part 5

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -1,10 +1,25 @@
-import { FlatList } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { Tile } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const DirectoryScreen = ({ navigation }) => {
   const campsites = useSelector((state) => state.campsites);
+
+  if (campsites.isLoading) {
+    return <Loading />;
+  }
+  if (campsites.errMess) {
+    return (
+      <View>
+        <Text>{campsites.errMess}</Text>
+        <Text>Troubleshooting:</Text>
+        <Text> Did you start the JSON server?</Text>
+        <Text>Check your IP. Is it right in the BaseUrl?</Text>
+      </View>
+    );
+  }
 
   const renderDirectoryItem = ({ item: campsite }) => {
     return (
